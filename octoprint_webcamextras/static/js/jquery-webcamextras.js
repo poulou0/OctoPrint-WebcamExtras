ca3a003e5d6c64d0408a42363db6aae54c83f31d
poulou0 <poulou.0@gmail.com>
2023-04-23 15:10:44 +0200

    octoprint 1.9.0 fixes

--- octoprint_webcamextras/static/js/jquery-webcamextras.js
+++ octoprint_webcamextras/static/js/jquery-webcamextras.js
@@ -1,10 +1,11 @@
 $(function () {
+    var webcam_container_selector = $("#webcam_container").length === 0 ? "#webcam_img_container" : "#webcam_container";
     function WebcamExtrasViewModel(parameters) {
         this.onStartupComplete = function () {
 
             var fullscreen_handler = function (e) {
                 if (!document.fullscreenElement) {
-                    var elem = $(e.currentTarget).parents("#webcam_container").get(0);
+                    var elem = $(e.currentTarget).parents(webcam_container_selector).get(0);
                     if (!elem) elem = $(e.currentTarget).parents("#IUCWebcamContainer").get(0);
                     var req = elem.requestFullScreen || elem.webkitRequestFullScreen || elem.mozRequestFullScreen;
                     req.call(elem);
@@ -15,13 +16,13 @@ $(function () {
                 }
             };
 
-            $("#webcam_container").before($('<div class="webcam-extras-floating-window-placeholder">' +
+            $(webcam_container_selector).before($('<div class="webcam-extras-floating-window-placeholder">' +
                 '<span class="fa-stack fa-4x"><i class="fas fa-expand fa-stack-2x"></i><i class="fas fa-window-restore fa-stack-1x"></i></span></div>'));
-            if (!$("#webcam_container").find(".UICWebCamClick").length) $("#webcam_container").append('<div class="webcam-extras"></div>');
+            if (!$(webcam_container_selector).find(".UICWebCamClick").length) $(webcam_container_selector).append('<div class="webcam-extras"></div>');
             if (!$("#IUCWebcamContainer").find(".UICWebCamClick").length) $("#IUCWebcamContainer").append('<div class="webcam-extras"></div>');
-            $("#webcam_container .webcam-extras").append($('<button class="btn webcam-extras-floating-window"><i class="fas fa-window-restore fa-flip-horizontal"></i></button>'));
+            $(webcam_container_selector + " .webcam-extras").append($('<button class="btn webcam-extras-floating-window"><i class="fas fa-window-restore fa-flip-horizontal"></i></button>'));
             $(".webcam-extras").append($('<button class="btn btn-primary webcam-extras-fullscreen"><i class="fas fa-expand"></i></button>'));
-            $(document).on("click", "#webcam_rotator, #IUCWebcamContainerInner, #UICWebCamFull", function (e) {
+            $(document).on("click", "#webcam_rotator, .webcam_unrotated, #IUCWebcamContainerInner, #UICWebCamFull", function (e) {
                 var img_wrapper = $(this);
                 var img = img_wrapper.find("img");
                 if (!img_wrapper.hasClass("zoomed-in")) {
@@ -58,12 +59,12 @@ $(function () {
                 img_wrapper.toggleClass("zoomed-in");
             });
             $('.webcam-extras-fullscreen').on("click", fullscreen_handler);
-            if (!$("#webcam_container").find(".UICWebCamClick").length) $('#webcam_rotator').on("dblclick", fullscreen_handler);
+            if (!$(webcam_container_selector).find(".UICWebCamClick").length) $('#webcam_rotator').on("dblclick", fullscreen_handler);
             if (!$("#IUCWebcamContainer").find(".UICWebCamClick").length) $('#IUCWebcamContainerInner').on("dblclick", fullscreen_handler);
             $('.webcam-extras-floating-window, .webcam-extras-floating-window-placeholder .fa-stack').on("click", function () {
-                var relative = $('#control #webcam_container');
+                var relative = $('#control ' + webcam_container_selector);
                 var placeholder = $('.webcam-extras-floating-window-placeholder').show();
-                if (!$('body > #webcam_container').length) {
+                if (!$('body > ' + webcam_container_selector).length) {
                     if (document.fullscreenElement) document.exitFullscreen();
                     relative.hide().clone(true, true).appendTo('body')
                         .fadeIn()
@@ -99,7 +100,7 @@ $(function () {
                     relative.next().css('opacity', 0);
                     placeholder.show();
                 } else {
-                    $('body > #webcam_container').remove();
+                    $('body > ' + webcam_container_selector).remove();
 
                     relative.fadeIn().next().css('opacity', 1);
                     placeholder.hide();
@@ -108,9 +109,17 @@ $(function () {
         };
     }
 
-    OCTOPRINT_VIEWMODELS.push([
-        WebcamExtrasViewModel,
-        ["controlViewModel"],
-        ["#webcam_container"]
-    ]);
+    if ($("#webcam_container").length === 0) {
+        OCTOPRINT_VIEWMODELS.push([
+            WebcamExtrasViewModel,
+            ["controlViewModel"],
+            ["#webcam_container"]
+        ]);
+    } else {
+        OCTOPRINT_VIEWMODELS.push([
+            WebcamExtrasViewModel,
+            ["controlViewModel"],
+            ["#webcam_img_container"]
+        ]);
+    }
 });
